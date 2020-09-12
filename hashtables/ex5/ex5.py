525cdc3eb07abcc720ea9c02f917b5f1d7306199
unknown <m_fattih@hotmail.com>
2020-09-11 20:29:50 -0700

    ex 1,4 completed

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,21 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    store = {}
 
-    return result
+    store = dict.fromkeys(files, 0)
+    # for i in dict.keys():
+    #     split_version = i.split('/')[-1]
+    #     # print(split_version, i)
+    #     if split_version in queries:
+    #         store[i] = 1
+    # # return list(store.keys())
+    return list(dict(filter(lambda x: x[0].split('/')[-1] in queries, store.items())).keys())
 
 
 if __name__ == "__main__":
